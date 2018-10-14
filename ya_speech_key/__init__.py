a7ed838d6ca9f768aa580c5f807a3a41344543c2
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-10-14 21:13:32 +0300

    Raise Oops if request blocked

--- ya_speech_key/__init__.py
+++ ya_speech_key/__init__.py
@@ -2,16 +2,27 @@
 
 import requests
 import time
-import random
+
+
+class Oops(Exception):
+    pass
 
 
 class APIKey:
     REQUEST_ERRORS = (requests.exceptions.HTTPError, requests.exceptions.RequestException)
-    URLS = [
-        'translate.yandex.ru', 'translate.yandex.ua', 'translate.yandex.by',
-        'translate.yandex.kz', 'ceviri.yandex.com.tr', 'translate.yandex.com'
-            ]
+    URL = 'https://translate.yandex.com'
     TARGET = 'SPEECHKIT_KEY:'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,ru;q=0.7,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache'
+    }
 
     def __init__(self, lifetime=3600):
         self._key = None
@@ -28,24 +39,20 @@ class APIKey:
         return time.time() > self._by_expire
 
     def _get_key(self):
-        key = None
-        for target in self.URLS:
-            key = self._extract('https://{}'.format(target))
-            if not key:
-                time.sleep(random.random())
-            else:
-                break
+        key = self._extract()
         if not key:
             raise RuntimeError('API Key not extracted. Yandex change page?')
         self._by_expire = time.time() + self._lifetime
         self._key = key
 
-    def _extract(self, url):
+    def _extract(self):
         try:
-            response = requests.get(url)
+            response = requests.get(self.URL, headers=self.HEADERS)
         except self.REQUEST_ERRORS as e:
             raise RuntimeError(e)
         line = response.text
+        if line.find('<title>Oops!</title>') > -1:
+            raise Oops('Yandex blocked automated requests')
         end = 0
         result = None
         start = line.find(self.TARGET)
--- ya_speech_key/tests/tests.py
+++ ya_speech_key/tests/tests.py
@@ -2,13 +2,17 @@
 
 import unittest
 
-from ya_speech_key import APIKey
+from ya_speech_key import APIKey, Oops
 
 
 class Tests(unittest.TestCase):
     def test_01(self):
-        key = APIKey()
-        print(key.key)
+        try:
+            key = APIKey()
+            print(key.key)
+        except Oops as e:
+            print('Impossible to test: {}'.format(e))
+            return
         self.assertEqual(key.key, key.key)
         self.assertTrue('-' in key.key)
         self.assertTrue(' ' not in key.key)
